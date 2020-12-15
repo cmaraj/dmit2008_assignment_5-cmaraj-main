918354fcd284b05bb42ee35fe26f25ea18310f52
cmaraj <cameron.maraj@hotmail.ca>
2020-12-15 16:01:39 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+//import path from 'path';
+
+module.exports = {
+    entry: './js/src/main.js',
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'build')
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(m?js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                }, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: './index.html',
+            favicon: './favicon.ico'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 9001
+    }
+}
